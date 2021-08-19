fac2563dd9b6365ed7ad06495cc27ee1604696c6
henriquecesar139 <henriquepb139@gmail.com>
2021-08-19 17:33:46 -0300

    Arquivo principal

---
+++ calculadora.py
@@ -0,0 +1,25 @@
+from calculos import *
+
+print('''
+____ ____ _    ____ _  _ _    ____ ___  ____ ____ ____ 
+|    |__| |    |    |  | |    |__| |  \ |  | |__/ |__| 
+|___ |  | |___ |___ |__| |___ |  | |__/ |__| |  \ |  |                                                    
+
+[ 0 ] --> Sair
+[ 1 ] --> Adição
+[ 2 ] --> Subtração
+[ 3 ] --> Multiplicação
+[ 4 ] --> Divisão
+
+''')
+
+conta = int (input('---> '))
+
+if conta == 1:
+    adicao()
+if conta == 2:
+    subtracao()
+if conta == 3:
+    multiplicacao()
+if conta == 4:
+    divisao()
