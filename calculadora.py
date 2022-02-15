7527ad9a47a642e10cbbf538122f54c79d6561b1
henriquecesar139 <henriquecesar1395@gmail.com>
2022-02-14 21:20:40 -0300

    otimização

--- calculadora.py
+++ calculadora.py
@@ -1,42 +1,31 @@
-from calculos import *
+from math import *
+
+def calcular(conta):
+    return eval(conta)
+
 
 print('''
 ____ ____ _    ____ _  _ _    ____ ___  ____ ____ ____ 
 |    |__| |    |    |  | |    |__| |  \ |  | |__/ |__| 
 |___ |  | |___ |___ |__| |___ |  | |__/ |__| |  \ |  |                                                    
 
-[ 0 ] --> Sair
-[ 1 ] --> Adição
-[ 2 ] --> Subtração
-[ 3 ] --> Multiplicação
-[ 4 ] --> Divisão
-[ 5 ] --> Exponenciação
-[ 6 ] --> Radiciação
-[ 7 ] --> Porcentagem
-[ 8 ] --> Permilagem
+ _____________________
+|  _________________  |
+| |    calculadora  | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | * | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |   
+|_____________________|
+
 ''')
 
-conta = int (input('---> '))
-
-if conta == 1:
-    adicao()
-    (exit())
-if conta == 2:
-    subtracao()
-    (exit())
-if conta == 3:
-    multiplicacao()
-if conta == 4:
-    divisao()
-    (exit())
-if conta == 5:
-    exponenciacao()
-if conta == 6:
-    radiciacao()
-    (exit())
-if conta == 7:
-    porcentagem()
-    (exit())
-if conta == 8:
-    permilagem()
-    (exit())
+while True:
+    conta = input('[conta]==-> ')
+    print(calcular(conta))
